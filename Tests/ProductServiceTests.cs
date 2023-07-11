b12840d6a7b56a806f257cffb3b8b37508d159cb
Vasya Ryzhkoff <ryzh.v.n@mail.ru>
2023-07-11 18:22:39 +0700

    new productService UnitTest

--- Tests/ProductServiceTests.cs
+++ Tests/ProductServiceTests.cs
@@ -5,6 +5,8 @@ using MagazinchikAPI.Validators;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Moq.EntityFrameworkCore;
+using AutoMapper;
+using MagazinchikAPI.DTO;
 
 namespace Tests;
 
@@ -12,12 +14,14 @@ public class ProductServiceTests
 {
 
     private readonly MagazinchikAPI.Services.ProductService _service;
-    private readonly Mock<AutoMapper.IMapper> _mapperMock = new();
+    private readonly IMapper _mapper;
     private Mock<ApplicationDbContext> _contextMock = new();
 
     public ProductServiceTests()
     {
-        _service = new ProductService(_contextMock.Object, _mapperMock.Object, new ReviewDtoCreateValidator(), new ReviewDtoUpdateValidator());
+        var mapperConfig = new MapperConfiguration(x => x.AddProfile(new ApplicationProfile()));
+        _mapper = new Mapper(mapperConfig);
+        _service = new ProductService(_contextMock.Object, _mapper, new ReviewDtoCreateValidator(), new ReviewDtoUpdateValidator());
     }
 
 
@@ -39,5 +43,43 @@ public class ProductServiceTests
 
         var exception = await Assert.ThrowsAsync<APIException>(act);
         Assert.Equal(404, exception.StatusCode);
+    }
+
+
+
+    [Fact]
+    public async Task GetBaseInfo_ShouldReturnProductDto_IfExists()
+    {
+        //Arrange
+        var productId = new Random().NextInt64(long.MaxValue);
+
+        var fakeProducts = new List<Product>
+        {
+            new Product()
+            {
+                Id = productId,
+                Name = "TestProduct",
+                Price = 123.0M,
+                Description = "helloworld"
+            }
+        };
+
+        var fakeResult = _mapper.Map<ProductDtoBaseInfo>(fakeProducts[0]);
+
+        _contextMock.Setup<DbSet<Product>>(x => x.Products)
+        .ReturnsDbSet(fakeProducts);
+
+
+
+        //Act
+        var result = await _service.GetBaseInfo(productId);
+
+
+        //Assert
+        Assert.IsType<MagazinchikAPI.DTO.ProductDtoBaseInfo>(result);
+        Assert.Equal(result.Id, fakeResult.Id);
+        Assert.Equal(result.Name, fakeResult.Name);
+        Assert.Equal(result.Price, fakeResult.Price);
+        Assert.Equal(result.Description, fakeResult.Description);
     }
 }
