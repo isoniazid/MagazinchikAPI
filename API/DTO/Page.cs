33e47bbf2d8936a77f4e20586987129a27eed91c
Vasya Ryzhkoff <ryzh.v.n@mail.ru>
2023-07-16 19:30:55 +0700

    jsonProperties new names in Page dto

--- API/DTO/Page.cs
+++ API/DTO/Page.cs
@@ -1,10 +1,20 @@
+using System.Text.Json.Serialization;
+
 namespace MagazinchikAPI.DTO
 {
     public class Page<T>
     {
-        public int Pages {get; set;}
-        public int CurrentOffset {get; set;}
+        public int Pages { get; set; }
 
-        public List<T> CurrentPage {get; set;} = new();
+        [JsonIgnore]
+        public int CurrentOffset { get; set; }
+
+        private List<T> _currentPage = new();
+
+        [JsonPropertyName("rows")]
+        public List<T> CurrentPage { get => _currentPage; set => (_currentPage, ElementsCount) = (value, value.Count); }
+
+        [JsonPropertyName("count")]
+        public int ElementsCount { get; set; }
     }
 }
