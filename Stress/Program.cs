fa758bb1b19d62f53a502fe79e434d5245974f0b
Vasya Ryzhkoff <ryzh.v.n@mail.ru>
2023-07-22 00:27:39 +0700

    added simple stress-test

---
+++ Stress/Program.cs
@@ -0,0 +1,29 @@
+﻿using NBomber.CSharp;
+using NBomber.Configuration;
+using NBomber.Contracts;
+using NBomber.Http.CSharp;
+
+class Program
+{
+    static void Main()
+    {
+        using var httpClient = new HttpClient();
+
+        var scenario = Scenario.Create("Get All Products", async context =>
+        {
+            var response = await httpClient.GetAsync("http://localhost:5000/api/product/get-all?limit=50&page=0");
+
+            return response.IsSuccessStatusCode
+            ? Response.Ok()
+            : Response.Fail();
+
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.Inject(rate: 1000, interval: TimeSpan.FromSeconds(1),
+            during: TimeSpan.FromSeconds(30))
+        );
+
+        NBomberRunner.RegisterScenarios(scenario).Run();
+    }
+}
